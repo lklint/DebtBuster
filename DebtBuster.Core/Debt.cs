96604dcc8f26c72a620823eca26dfb0a1a8953b7
lklint <lars@klint.co>
2013-11-18 12:28:01 +1100

    Initial Commit

---
+++ DebtBuster.Core/Debt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DebtBuster.Core
+{
+    public class Debt
+    {
+        public decimal Interest { get; set; }
+        public DateTime TermEnd { get; set; }
+        public DateTime TermStart { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
